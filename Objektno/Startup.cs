c747e014d9e689f3977883b1b12d8348936c1b70
Petar Stojanović <petar.stojanovic@fer.hr>
2016-12-27 13:09:36 +0100

    Add project files.

---
+++ Objektno/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Objektno.Startup))]
+namespace Objektno
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
