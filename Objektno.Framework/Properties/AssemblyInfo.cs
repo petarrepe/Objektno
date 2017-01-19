e527963dd5883c73af9da9c5186d6160e9f0c185
Cottiero <ivan.cottiero@fer.hr>
2017-01-19 17:23:22 +0100

    Preimenovan Framework iz Objektno u KonobApp

--- Objektno.Framework/Properties/AssemblyInfo.cs
+++ Objektno.Framework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Objektno.Framework")]
+[assembly: AssemblyTitle("KonobApp.Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Objektno.Framework")]
+[assembly: AssemblyProduct("KonobApp.Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
