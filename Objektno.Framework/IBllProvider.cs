851842060c5cdd9b1f88ee20cc928700d14d548c
Cottiero <ivan.cottiero@fer.hr>
2017-01-19 17:21:40 +0100

    Dodan framework za BLL i DAL projekte

---
+++ Objektno.Framework/IBllProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace Objektno.Framework
+{
+    public interface IBllProvider
+    {
+        void Validate(object businessObjet, string propertyName);
+        void SaveChanges(IList changes);
+        void Save(object busiessObject);
+    }
+}
---
+++ Objektno.Framework/IBusinessObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektno.Framework
+{
+    public interface IBusinessObject
+    {
+        bool IsDirty { get; }
+        BusinessObjectState State { get; }
+        bool InEdit { get; }
+
+        void Load(IDTOObject dalRecord);
+        IDTOObject ToDtoObject();
+
+        void Delete();
+        void Edit();
+
+        void CancelChanges();
+        void AcceptChanges();
+
+        void ClearErrors();
+        void Validate(string propertyName);
+
+        void SetParent(IBusinessObjectList parent);
+        IBllProvider BllProvider { get; set; }
+    }
+}
