42b1b90dfc2e9127b74ddfeb2a7dfd5de60e1788
SvenKolaric <fluffycro@gmail.com>
2017-01-26 11:30:22 +0100

    Popravljena greška u mappingu

--- DAL/Repositories/IArticleRepository.cs
+++ DAL/Repositories/IArticleRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Models;
 
 namespace DAL.Repositories
 {
     public interface IArticleRepository
     {
+        ArticleModel Article { get; }
+        CategoryModel Category { get; }
+
+
 
     }
 }
