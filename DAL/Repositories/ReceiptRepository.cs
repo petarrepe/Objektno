cdb8d2bbe6b57a9b415eb5bd7e7e24e9e1fa97c5
Cottiero <ivan.cottiero@fer.hr>
2017-01-26 13:24:26 +0100

    Izmjenjeni Namespace-ovi uModel projektu, dodan ReceiptRepository u DAL-u

---
+++ DAL/Repositories/ReceiptRepository.cs
@@ -0,0 +1,16 @@
+﻿using KonobApp.Interfaces;
+using KonobApp.Model.Models;
+using KonobApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    class ReceiptRepository : Subject, IReceiptRepository
+    {
+        private IList<Receipt>
+    }
+}
--- KonobApp.Model/Repositories/IReceiptRepository.cs
+++ KonobApp.Model/Repositories/IReceiptRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KonobApp.Model.Repositories
+namespace KonobApp.Model
 {
     public interface IReceiptRepository
     {
