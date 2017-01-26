e77a4cd60643c43216993135b5954563179ffdc1
SvenKolaric <fluffycro@gmail.com>
2017-01-26 12:45:53 +0100

    dodani interface za caffe i article

---
+++ DAL/Repositories/ICaffeRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL.Repositories
+{
+    public interface ICaffeRepository
+    {
+        IList<TableModel> Tables { get; }
+        IList<WaiterModel> Waiters { get; }
+        IList<ArticleInCaffeModel> ArticlesInCaffe { get; }
+
+        void LoadAll();
+        void LoadCaffe();
+        void LoadTables();
+        void LoadWaiters();
+        void LoadArticlesInCaffe();
+
+        void AddCaffe(string name, string address, bool isOpen);
+        void UpdateCaffe(int caffeID, string name, string address, bool isOpen);
+        void DeleteCaffe(int caffeID);
+
+        void AddArticleInCaffe(int articleID, int caffeID, bool isAvailabe);
+    }
+}
