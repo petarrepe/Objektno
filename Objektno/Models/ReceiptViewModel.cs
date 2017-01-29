5d280252de6e6ed32fff745b533ff0b53d695886
Petar Stojanović <petar.stojanovic@fer.hr>
2017-01-29 20:20:39 +0100

    dosta za dns

--- Objektno/Models/ReceiptViewModel.cs
+++ Objektno/Models/ReceiptViewModel.cs
@@ -6,13 +6,29 @@ namespace Objektno.Models
 {
     public class ReceiptViewModel
     {
-        private List<ArticleModel> articlesInCaffe;
+        public List<ArticleModel> articlesInCaffe;
+        public IDictionary<int, string> categories = new Dictionary<int,string>();
+
 
         internal ReceiptViewModel(int id)
         {
             var caffeRepository = DAL.Repositories.CaffeRepository.GetInstance();
 
-            articlesInCaffe = caffeRepository.ListArticlesInCaffe(id).ToList();
+            articlesInCaffe = caffeRepository.ListAvailableArticlesInCaffe(id).ToList();
+            var aviableCategoriesInCaffe = articlesInCaffe.Select(t => t.IDCategory).Distinct().ToList();
+
+            using (DAL.Facade facade = new DAL.Facade())
+            {
+                var temp = facade.FetchAll<CategoryModel>();
+                foreach(var value in temp)
+                {
+                    if (aviableCategoriesInCaffe.Contains(value.IDCategory))
+                    {
+                        categories.Add(new KeyValuePair<int, string>(value.IDCategory, value.Name));
+                    }
+                }
+            }
         }
+
     }
 }
